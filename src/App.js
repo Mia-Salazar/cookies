bbc75ef44f828a649b94ef0483cd181805e8e21a
Mia-Salazar <maria.sgr@gmail.com>
2024-04-03 18:23:35 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Mia Salazar</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Home } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} >
     <App />
+    </RouterProvider>
+
   </React.StrictMode>
 );
 
