c269b0f50e6cd868d818baf921e63d42c3812b8a
Mia-Salazar <maria.sgr@gmail.com>
2024-04-09 17:45:08 +0200

    Creating container

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import PropTypes from 'prop-types';
+
+import "./Container.scss";
+
+export const Container = ({children}) => {
+	return (
+		<div className="container">{children}</div>
+	);
+};
+
+Container.propTypes = {
+	children: PropTypes.node.isRequired,
+}
+
+export default Container;
