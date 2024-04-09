2a016258c3ae227614d0b9aa51b23f762e6db52f
Mia-Salazar <maria.sgr@gmail.com>
2024-04-09 18:59:16 +0200

    Adapt heading

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types';
 
 import "./Heading.scss";
 
-export const Heading = ({text}) => {
+export const Heading = ({text, type}) => {
 	return (
-		<div className="heading">{text}</div>
+		<div className={"heading " + (type > 1 ? `heading--h${type}` : '')}>{text}</div>
 	);
+};
+
+Heading.defaultProps = {
+	type: 1
 };
 
 Heading.propTypes = {
 	text: PropTypes.string.isRequired,
+    type: PropTypes.number
 }
 
 export default Heading;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,8 @@ export const Home = () => {
                 <Text>En 2018 empecé en informática y llevo enamorada desde entonces. Actualmente me estoy especializando en accesibilidad, un tema que me apasiona.
                     Soy una persona inquieta, a la que le gusta meterse 'fregaos' y que siempre está aprendiendo algo nuevo.</Text>
                 <Text>Friki por naturaleza, cuando no estoy programando algo en mi tiempo libre, veo series, juego a juegos de mesa o de rol o voy al cine.</Text>
+                <Heading text="Algunos fregaos" type={2} />
+
             </Container>
         </Layout>
 	);
