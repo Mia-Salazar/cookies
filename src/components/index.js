8c838491982c919bf3a2fe06c8c1d003339e898c
Mia-Salazar <maria.sgr@gmail.com>
2024-04-11 17:11:57 +0200

    Fix import error

--- src/components/index.js
+++ src/components/index.js
@@ -5,10 +5,10 @@ export { default as Container } from './atoms/Container/Container';
 export { default as Text } from './atoms/Text/Text';
 export { default as StrengthItem } from './atoms/StrengthItem/StrengthItem';
 export { default as SocialNetwork } from './atoms/SocialNetwork/SocialNetwork';
+export { default as Heading } from './atoms/Heading/Heading';
 
 export { default as Layout } from './molecules/Layout/Layout';
 export { default as Header } from './molecules/Header/Header';
-export { default as Heading } from './molecules/Header/Header';
 
 export { default as Navbar } from './organisms/Navbar/Navbar';
 export { default as Footer } from './organisms/Footer/Footer';
