26a781a1ab13b16dda435869dc67edc12200acce
Mia-Salazar <maria.sgr@gmail.com>
2024-04-11 20:07:22 +0200

    Add form functionality

--- src/components/organisms/Form/Form.jsx
+++ src/components/organisms/Form/Form.jsx
@@ -1,39 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-// import { emailValidator, textValidator } from "../../../utils/functionalities/validators";
-// import { Button } from "../../atoms/Button/Button";
-// import { FeedbackText } from "../../atoms/FeedbackText/FeedbackText";
-// import { InputGroup } from "../../molecules/InputGroup/InputGroup";
 import "./Form.scss";
-import Input from "../../atoms/Input/Input";
+
 import FormElement from "../../molecules/FormElement/FormElement";
 import Button from "../../atoms/Button/Button";
 
 export const Form = () => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [textarea, setTextarea] = useState("");
-	const [inputValidation, setInputValidation] = useState({
-		name: null,
-		email: null,
-		textarea: null
-	});
+    const [values, setValues] = useState({name: "", email:"", street: "", city: "", country: "", comment: ""});
 	const [serverState, setServerState] = useState({
 		submitting: false,
 		status: null
 	});
+
 	const handleServerResponse = (ok, msg) => {
 		setServerState({
 			submitting: false,
 			status: { ok, msg }
 		});
 		if (ok) {
-			setEmail("");
-			setTextarea("");
-			setName("");
+            setValues({
+                name: "",
+                email:"",
+                street: "",
+                city: "",
+                country: "",
+                comment: "",
+
+            })
 		}
 	};
+
 	const handleOnSubmit = e => {
 		e.preventDefault();
 		const form = e.target;
@@ -41,40 +38,31 @@ export const Form = () => {
 
         axios.post('https://formspree.io/f/mjvjpnrl', new FormData(form))
 			.then(() => {
+                //this
 				handleServerResponse(true, "contact.success");
 			})
 			.catch(r => {
 				handleServerResponse(false, r.response.data.error);
 			});
 	};
+
 	const inputChange = (param) => (e) => {
-		const inputValue = e.target.value;
-		if (param === "email") {
-			setEmail(inputValue);
-		}
-		if (param === "textarea") {
-			setTextarea(inputValue);
-		}
-		if (param === "name") {
-			setName(inputValue);
-		}
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+
 	};
-	useEffect(() => {
-		// setInputValidation({
-		// 	email: emailValidator(email),
-		// 	name: textValidator(name),
-		// 	textarea: textValidator(textarea)
-		// });
-	}, [email, name, textarea]);
+
 	return (
 		<form className="form" onSubmit={handleOnSubmit}>
-            <FormElement type="text" id="name" text="Nombre" value={name} functionality={inputChange("name")} />
-            <FormElement type="email" id="name" text="Email" value={name} functionality={inputChange("name")} />
-            <FormElement type="text" id="name" text="Calle" value={name} functionality={inputChange("name")} />
-            <FormElement type="text" id="name" text="Ciudad" value={name} functionality={inputChange("name")} />
-            <FormElement type="text" id="name" text="País" value={name} functionality={inputChange("name")} />
-            <FormElement  id="name" text="Mensaje" value={name} functionality={inputChange("name")} />
-			<Button text="Enviar" disabled={serverState.submitting || email === undefined || textarea === undefined} type="submit"/>
+            <FormElement type="text" id="name" text="Nombre" value={values.name} functionality={inputChange("name")} />
+            <FormElement type="email" id="email" text="Email" value={values.email} functionality={inputChange("email")} />
+            <FormElement type="text" id="street" text="Calle" value={values.street} functionality={inputChange("street")} />
+            <FormElement type="text" id="city" text="Ciudad" value={values.city} functionality={inputChange("city")} />
+            <FormElement type="text" id="country" text="País" value={values.country} functionality={inputChange("country")} />
+            <FormElement  id="comment" text="Mensaje" value={values.comment} functionality={inputChange("comment")} />
+			<Button text="Enviar" disabled={serverState.submitting || !values.email === "" || values.comment === ""} type="submit"/>
 		</form>
 	);
 };
