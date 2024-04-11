4dc5b32d611c31fe3ddefb7daf23ca30395ad5b8
Mia-Salazar <maria.sgr@gmail.com>
2024-04-11 17:28:17 +0200

    Fix imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,9 @@
+export { default as Navbar } from './organisms/Navbar/Navbar';
+export { default as Footer } from './organisms/Footer/Footer';
+
+export { default as Layout } from './molecules/Layout/Layout';
+export { default as Header } from './molecules/Header/Header';
+
 export { default as ButtonLink } from './atoms/ButtonLink/ButtonLink';
 export { default as CloudItem } from './atoms/CloudItem/CloudItem';
 export { default as Cookie } from './atoms/Cookie/Cookie';
@@ -7,11 +13,9 @@ export { default as StrengthItem } from './atoms/StrengthItem/StrengthItem';
 export { default as SocialNetwork } from './atoms/SocialNetwork/SocialNetwork';
 export { default as Heading } from './atoms/Heading/Heading';
 
-export { default as Layout } from './molecules/Layout/Layout';
-export { default as Header } from './molecules/Header/Header';
 
-export { default as Navbar } from './organisms/Navbar/Navbar';
-export { default as Footer } from './organisms/Footer/Footer';
+
+
 
 
 
