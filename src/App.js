bea920d1625be928a220b680397427c6196f9429
Mia-Salazar <maria.sgr@gmail.com>
2024-04-11 18:36:43 +0200

    Add new route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
-import { Home, Intro } from './pages';
+import { Contact, Home, Intro } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/" element={<Intro />} />
       </Routes>
