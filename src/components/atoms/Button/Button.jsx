7575c09bd9dd6b1f3c7dc5bd5c7223b8bcbba8ff
Mia-Salazar <maria.sgr@gmail.com>
2024-04-12 16:03:28 +0200

    Add secondary button

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 import "./Button.scss";
 
-export const Button = ({ text, type = "button", functionality, disabled = false, ...rest}) => {
+export const Button = ({ text, type = "button", functionality, disabled = false, styles, ...rest}) => {
 	return (
-		<button {...rest} className="button" onClick={functionality} type={type} disabled={disabled}>{text}</button>
+		<button {...rest} className={`button ${styles ? `button--${styles}` : ''}`} onClick={functionality} type={type} disabled={disabled}>{text}</button>
 	);
 };
 
@@ -13,7 +13,8 @@ Button.propTypes = {
 	text: PropTypes.string.isRequired,
 	type: PropTypes.string,
 	functionality: PropTypes.func,
-	disabled: PropTypes.bool
+	disabled: PropTypes.bool,
+	styles:  PropTypes.string
 };
 
 export default Button;
