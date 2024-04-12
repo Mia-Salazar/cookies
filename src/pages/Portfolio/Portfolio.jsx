53310ebc40ae813287d2eb7b497164bae24e3483
Mia-Salazar <maria.sgr@gmail.com>
2024-04-12 18:31:17 +0200

    Add old protfolios and styles

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { oldPortfolio } from "../../utils/data/PortfolioElements";
 import { 
         Container,
         Header,
@@ -10,7 +11,6 @@ import {
         PortfolioItem,
         Text 
     } from "../../components";
-    import foodiesaurus from "../../assets/img/foodiesaurus.png";
 import "./Portfolio.scss";
 
 export const Portfolio = () => {
@@ -22,6 +22,18 @@ export const Portfolio = () => {
                 <Heading text="Portfolio" />
                 <Text>Aquí puedes ver algunos de los proyectos en los que he trabajado a nivel profesional, y muchos de los proyectos personales que he realizado</Text>
                 <PortfolioContent />
+                <Heading text="Mi pasado" type={2} />
+                <Text>Antes de ser una monstrua de las galletas, fui una viajera intergaláctica y una superheroína. Puedes ver mis proyectos aquí</Text>
+                <div className="portfolio__list">
+                    {
+                        oldPortfolio.map(({text, image, href}, index) => {
+                            return(
+                                <PortfolioItem text={text} href={href} image={image} key={index}/>
+                            );
+                        })
+                    }
+                </div>
+                
             </Container>
         </Layout>
 	);
