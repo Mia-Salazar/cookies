e207e5c06904259f92d78241bd61c5a299fef01c
Mia-Salazar <maria.sgr@gmail.com>
2024-04-12 15:32:19 +0200

    Change array

---
+++ src/utils/data/PortfolioElements.js
@@ -0,0 +1,45 @@
+import lotr from "../../assets/img/lotr.jpg";
+import spotiapp from "../../assets/img/spotiapp.jpg";
+import movie from "../../assets/img/movie.jpg";
+import crusha from "../../assets/img/crusha.jpg";
+import wizard from "../../assets/img/wizard.jpg";
+import todo from "../../assets/img/todo.jpg";
+import teresa from "../../assets/img/teresa.png";
+import movies from "../../assets/img/movies.png";
+import happets from "../../assets/img/happets-reviews.png";
+import happetsBis from "../../assets/img/happets-2.png";
+import game from "../../assets/img/game.png";
+import fleetLanding from "../../assets/img/fleeet-landing.png";
+import fleetCatalog from "../../assets/img/fleeet-catalog.png";
+import enthec from "../../assets/img/enthec.jpg";
+import kartos from "../../assets/img/kartos.png";
+import slot from "../../assets/img/slot.jpg";
+import xcjgames from "../../assets/img/xcjgames.PNG";
+import pollution from "../../assets/img/pollution.png";
+import me from "../../assets/img/me.png";
+import connect from "../../assets/img/connect.png";
+import foodiesaurus from "../../assets/img/foodiesaurus.png";
+
+export const portfolioArray = [
+	{ href: "https://foodiesaurus.com/", text:"HTML5, CSS3, Javascript, PHP, MySQL, phpMyAdmin", src: foodiesaurus },
+	{ href: "https://enthec.com", text:"Angular, HTML5, CSS3, SASS, Flexbox, Grid, Typescript, API Rest, i18n, BEM Methodology, Accesibility", src: enthec },
+	{ href: "https://kartos.enthec.com", text:"Angular, Angular Material, Redux, HTML5, CSS3, SASS, Flexbox, Grid, i18n, BEM Methodology, Typescript, API Rest, Accesibility", src: kartos },
+	{ href: "http://xcjgames.surge.sh/", text:"Reactjs, React Hooks, Jest, Testing-library, HTML5, CSS3, SASS, Flexbox, Grid, Javascript,", src: xcjgames },
+	{ href: "https://mia-salazar.github.io/quiz", text:"Reactjs, React Hooks, HTML5, CSS3, SASS, Flexbox, Grid, Javascript, BOOTSTRA.386", src: me },
+	{ href: "https://mia-salazar.github.io/reddit-searcher/", text:"Reactjs, React Hooks, Custom Hooks, API Rest, Nes CSS, HTML5, CSS3, SASS, Flexbox, Grid, Javascript,", src: pollution },
+	{ href: "http://teresa-salazar.surge.sh/", text:"Reactjs, Javascript, CSS3, HTML5, BEM, SASS, Grid, Flexbox, i18n, Classnames, librerías, Formspree, lazy load", src: teresa },
+	{ href: "https://mia-salazar.github.io/connect-4/", text:"Reactjs, Javascript, CSS3, HTML5, Grid, Flexbox, PaperCSS", src: connect },
+	{ href: "https://github.com/Mia-Salazar/shop-redux", text:"Angular, Typescript, Tailwind, Redux, API Rest, CSS3, HTML5, SASS, Grid, Flexbox", src: lotr },
+	{ href: "https://github.com/Mia-Salazar/spotiapp", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4", src: spotiapp },
+	{ href: "https://github.com/Mia-Salazar/media-searcher", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4, Materialize", src: movie },
+	{ href: "https://github.com/Mia-Salazar/youtube-angular", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4", src: crusha },
+	{ href: "https://github.com/Mia-Salazar/crud-heroes", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4, Materialize", src: wizard },
+	{ href: "https://github.com/Mia-Salazar/redux-todo", text:"Angular, Typescript, Redux, CSS3, HTML5, SASS, Grid, Flexbox", src: todo },
+	{ href: "http://mia-search-movies-project.surge.sh/", text:"Reactjs, Javascript, Bulma, CSS3, HTML5, CSS3, Flexbox", src: movies },
+	{ href: "https://www.happets.com/opiniones", text:"JavaScript, jQuery, SASS, HTML5, CSS3, Flexbox, Grid, Bootstrap 4", src: happets },
+	{ href: "https://www.happets.com/login", text:"Reactjs, React hooks, Gatsby, storybook, testing, i18n, JavaScript, SASS, HTML5, CSS3, Flexbox, Grid", src: happetsBis },
+	{ href: "http://mia-tic-tac-toe.surge.sh/", text:"Reactjs Hooks, styled components, i18n, proptypes, classnames, HTML5, CSS3, SASS", src: game },
+	{ href: "https://www.myfleeet.com/", text:"Vuejs, Nuxtjs, SASS, jQuery, HTML5, CSS3, Flexbox, BEM Methodology, Pug, vue-i18n", src: fleetLanding},
+	{ href: "https://www.myfleeet.com/client/catalog", text:"Vuejs, Nuxtjs, SASS, jQuery,HTML5, CSS3, Flexbox,BEM Methodology, Pug, vue-i18n", src: fleetCatalog},
+	{ href: "https://mia-salazar.github.io/slot-machine/", text:"HTML5, CSS3, Javascript, Flexbox", src: slot },
+];
