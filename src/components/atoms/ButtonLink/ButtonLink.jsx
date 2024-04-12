abef0f4e84cfd4368792c0fcb82643f2c0abc7e6
Mia-Salazar <maria.sgr@gmail.com>
2024-04-12 15:05:45 +0200

    Adapt button link to styles

--- src/components/atoms/ButtonLink/ButtonLink.jsx
+++ src/components/atoms/ButtonLink/ButtonLink.jsx
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 
 import "./ButtonLink.scss";
 
-export const ButtonLink = ({href, text}) => {
+export const ButtonLink = ({href, text, style}) => {
 	return (
-		<Link className={"link"} to={href}>{text}</Link>
+		<Link className={`link ${style ? `style--${style}` : ''}`} to={href}>{text}</Link>
 	);
 };
 
 ButtonLink.propTypes = {
 	href: PropTypes.string.isRequired,
+	style: PropTypes.string,
 	text: PropTypes.string.isRequired,
 }
 
