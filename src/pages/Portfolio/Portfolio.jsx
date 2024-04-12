e28e0d8e9b52d40908836d8030cb0b6682f340de
Mia-Salazar <maria.sgr@gmail.com>
2024-04-12 12:46:41 +0200

    Create portoflio comppnent

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { CloudItem, Container, Header, Heading, Layout, Navbar, StrengthItem, Text } from "../../components";
+import "./Portfolio.scss";
+
+export const Portfolio = () => {
+	return (
+		<Layout>
+            <Header />
+            <Navbar />
+            <Container>
+            </Container>
+        </Layout>
+	);
+};
+
+export default Portfolio;
