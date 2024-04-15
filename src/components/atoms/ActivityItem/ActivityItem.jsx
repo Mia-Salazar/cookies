0c3a7274edcc7ad4117e9c2d6156020e3aada83c
Mia-Salazar <maria.sgr@gmail.com>
2024-04-15 18:01:12 +0200

    Add elements

--- src/components/atoms/ActivityItem/ActivityItem.jsx
+++ src/components/atoms/ActivityItem/ActivityItem.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
-import { ButtonLink } from "../../atoms/ButtonLink/ButtonLink";
 import "./ActivityItem.scss";
 
-export const ActivityItem = ({item}) => {
+export const ActivityItem = ({aria, href, icon, text, year}) => {
+	const { t } = useTranslation();
+
 	return (
 		<div className="list-item">
-			<div className="list-item__icon-container">
-				<span className={`fas fa-${item.icon}`} aria-label={item.aria} title={item.aria}></span>
-				<span className="list-item__lang">{item.lang}</span>
-			</div>
-			<ButtonLink text={item.text} href={item.href} target={item.target} modificator="simple"/>
+			<span className={`fas fa-${icon}`} aria-label={aria} title={aria}></span>
+			<span className="list-item__year">{year}</span>
+			<a className="list-item__link" href={href} target="__blank">{t(text)}</a>
 		</div>
 	);
 };
 
 ActivityItem.propTypes = {
-	item: PropTypes.object.isRequired,
+	aria: PropTypes.string.isRequired,
+	href: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+	year: PropTypes.number.isRequired,
 };
 
 export default ActivityItem;
--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -1,19 +1,50 @@
 import React from "react";
-
-import { Container, Header, Heading, Layout, Navbar, Text } from "../../components";
-import "./Activities.scss";
 import { useTranslation } from "react-i18next";
+
+import { ActivityItem, Container, Header, Heading, Layout, Navbar, Text } from "../../components";
+import { gameArray, interviewsArray, speechesArray } from "../../utils/data/ActivitiesElements";
+import "./Activities.scss";
 
 export const Activities = () => {
     const { t } = useTranslation();
+
 	return (
 		<Layout styles="activities">
             <Header />
             <Navbar />
-            <Container>{t('footer')}
-                <p></p>
-                <Heading text="Fregaos" />
-                <Text>Aquí puedes ver todos los fregaos en los que me he metido: desde charlas a Game Jams</Text>
+            <Container>
+                <Heading text="nav.activities" />
+                <Text>{t('activities.text')}</Text>
+                <Heading text="activities.speeches" type={2} />
+                <div className="activities__list">
+                    {
+                        speechesArray.map(({aria, href, icon, lang, text, year}, index) => {
+                            return(
+                                <ActivityItem aria={aria} href={href} icon={icon} lang={lang} text={text} year={year} key={index}/>
+                            );
+                        })
+                    }
+                </div>
+                <Heading text="activities.interview" type={2} />
+                <div className="activities__list">
+                    {
+                        interviewsArray.map(({aria, href, icon, lang, text, year}, index) => {
+                            return(
+                                <ActivityItem aria={aria} href={href} icon={icon} lang={lang} text={text} year={year} key={index}/>
+                            );
+                        })
+                    }
+                </div>
+                <Heading text="activities.game" type={2} />
+                <div className="activities__list">
+                    {
+                        gameArray.map(({aria, href, icon, lang, text, year}, index) => {
+                            return(
+                                <ActivityItem aria={aria} href={href} icon={icon} lang={lang} text={text} year={year} key={index}/>
+                            );
+                        })
+                    }
+                </div>
             </Container>
         </Layout>
 	);
