d814bf00de2e494c80f880583574f9e451224aaf
Mia-Salazar <maria.sgr@gmail.com>
2024-04-16 14:49:26 +0200

    Add articles

---
+++ src/utils/data/ArticlesElements.js
@@ -0,0 +1,7 @@
+export const articlesArray = [	
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", text: "A Developer’s Nightmare: Web Accessibility", href: "https://medium.com/@marasalazar/developers-nightmare-web-accessibility-52b9a6890403"},
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", text: "Why, Icomoon, why?", href: "https://medium.com/@marasalazar/why-icomoon-why-15c2d78ec96e" },
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", text: "How to Structure CSS in Vue Like a Pro", href: "https://marasalazar.medium.com/how-to-structure-css-in-vue-like-a-pro-5475749cf5ea" },
+	{ lang: "es", aria: "awards.programming", icon: "file-alt", text: "Cómo dar soporte a IE y no morir en el intento", href: "https://marasalazar.medium.com/c%C3%B3mo-dar-soporte-a-ie-y-no-morir-en-el-intento-7049dcbbf43f" },
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", text: "To Angular Material or not to Angular Material?", href: "https://marasalazar.medium.com/to-angular-material-or-not-to-angular-material-940a616ac22d" },
+];
