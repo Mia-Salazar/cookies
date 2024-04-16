29f75abdb085b970c83cb6c940f3565b54b755a2
Mia-Salazar <maria.sgr@gmail.com>
2024-04-16 16:48:29 +0200

    Add dev.to articles

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+import cookie from '../../assets/img/logo.png';
 import { Heading, Text, Layout, ArticleItem } from "../../components";
 import { articlesArray } from "../../utils/ArticlesElements";
 import "./Articles.scss";
+import { articleConstructor } from "../../models/ArticlesModel";
 
 export const Articles = () => {
     const { t } = useTranslation();
 
-    const [contentArray, setContentArray] = useState([]);
-
+    const [content, setContent] = useState({
+        data: [],
+        isLoading: true
+    });
 
 	useEffect(() => {
 		fetch("https://dev.to/api/articles?username=miasalazar")
 			.then(response => response.json())
 			.then(data => data.map(articleConstructor))
-			.then(data => setContentArray(prevState => [...data, ...prevState]));
+			.then(data => setContent({isLoading: false, data}));
 	}, []);
 
 	return (
@@ -24,12 +28,20 @@ export const Articles = () => {
             <Text>{t('articles.text')}</Text>
             <Text>{t('articles.textTwo')}</Text>
             <Heading text="Dev.to" type={2} />
+            {content.isLoading
+                ? <img alt="" className="articles__img" src={cookie} />
+                : content.data.map(({date, tags, title, href}) => {
+                    return(
+                        <ArticleItem date={date} tags={tags} title={title} href={href} key={title}/>
+                    );
+                })
+            }            
             <Heading text="Medium" type={2} />
 
             {
 				articlesArray.map(({date, tags, title, href}) => {
 					return(
-						<ArticleItem date={date} tags={tags} title={title} href={href} />
+						<ArticleItem date={date} tags={tags} title={title} href={href} key={title}/>
 					);
 				})
 			}
