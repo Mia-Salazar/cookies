a6cdcc40acb785b950928e2e72fcacabba3cb889
Mia-Salazar <maria.sgr@gmail.com>
2024-05-16 17:13:35 +0200

    Improve accesibility of language changer

--- src/components/molecules/LanguageButton/LanguageButton.jsx
+++ src/components/molecules/LanguageButton/LanguageButton.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 import i18n from "../../../i18n";
 import "./LanguageButton.scss";
 
 export const LanguageButton = () => {
+	const { t } = useTranslation();
 	const [language, setLanguage] = useState("es");
 
 	const changeLanguage = (language) => {
@@ -14,20 +16,23 @@ export const LanguageButton = () => {
 	
 	return (
 		<form className="language">
-			<div className="language__container">
-				<label htmlFor="en" className={language === "en" ? "language__label language__label--active" : "language__label" } >
-					en
-				</label>
-				<input checked={language === "en"} className="language__input"
-					id="en" type="radio" name="lang" onChange={() => changeLanguage("en")}/>
-			</div>
-			<div className="language__container">
-				<label htmlFor="es" className={language === "es" ? "language__label language__label--active" : "language__label" } >
-					es	
-				</label>
-				<input checked={language === "es"} className="language__input"
-					id="es" type="radio" name="lang" onChange={() => changeLanguage("es")}/>
-			</div>
+			<fieldset className="language__fieldset">
+				<legend className="language__title" aria-hidden="false">{t('nav.lang')}</legend>
+				<div className="language__container">
+					<input checked={language === "en"} className="language__input"
+						id="en" type="radio" name="lang" onChange={() => changeLanguage("en")}/>
+					<label htmlFor="en" className={language === "en" ? "language__label language__label--active" : "language__label" } >
+						en
+					</label>
+
+
+					<input checked={language === "es"} className="language__input"
+						id="es" type="radio" name="lang" onChange={() => changeLanguage("es")}/>
+					<label htmlFor="es" className={language === "es" ? "language__label language__label--active" : "language__label" } >
+						es	
+					</label>
+				</div>
+			</fieldset>
 		</form>
 	);
 };
